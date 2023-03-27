0a1694b022e4e9a8e8454034e717ecce35153785
Aaron Klinker <aaron.klinker@sensortower.com>
2023-03-27 18:29:44 -0500

    Convert to full typescript module

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Contributor {
-  username: string;
-  profileUrl: string;
-  avatarUrl: string;
-}
-
-export function getContributors(options: { owner: string; repo: string }): Promise<Contributor[]>;
--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,19 @@
 import { ofetch } from "ofetch";
 
+export interface Contributor {
+  username: string;
+  profileUrl: string;
+  avatarUrl: string;
+}
+
 /**
  * Returns the username, profile url, and avatar URL for all contributors of a repo.
  */
-export async function getContributors({ owner, repo }) {
+export async function getContributors(options: {
+  owner: string;
+  repo: string;
+}): Promise<Contributor[]> {
+  const { owner, repo } = options;
   const res = await ofetch(`https://api.github.com/repos/${owner}/${repo}/contributors`);
 
   return res.map((user) => ({
